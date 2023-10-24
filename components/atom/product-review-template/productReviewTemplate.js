418f840842c40f80c6bbfcb3cbddf7bfe825a736
Stef533 <137123241+Stef533@users.noreply.github.com>
2023-10-24 20:41:32 +0200

    complete product review template part

---
+++ components/atom/product-review-template/productReviewTemplate.js
@@ -0,0 +1,45 @@
+var likeClicked = false; // Variabile per tenere traccia se il pulsante Like è stato cliccato
+var dislikeClicked = false; // Variabile per tenere traccia se il pulsante Dislike è stato cliccato
+
+function pushLike() {
+    var numLikeElement = document.getElementsByClassName('numLike')[0];
+    var numLike = parseInt(numLikeElement.textContent);
+    var likeSvg = document.getElementsByClassName('likeSvg')
+
+    if (!likeClicked) {
+        numLikeElement.textContent = numLike + 1;
+        likeClicked = true;
+        likeSvg.classList.add('active')
+
+        
+    } else {
+        numLikeElement.textContent = numLike - 1;
+        likeClicked = false;
+    }
+
+    // Resetta dislikeClicked dopo un clic su Like
+    if (dislikeClicked) {
+        dislikeClicked = false;
+    }
+}
+
+function pushDislike() {
+    var numDislikeElement = document.getElementsByClassName('numDislike')[0];
+    var numDislike = parseInt(numDislikeElement.textContent);
+
+    if (!dislikeClicked) {
+        numDislikeElement.textContent = numDislike + 1;
+        dislikeClicked = true;
+
+    } else {
+        numDislikeElement.textContent = numDislike - 1;
+        dislikeClicked = false;
+    }
+
+    // Resetta likeClicked dopo un clic su Dislike
+    if (likeClicked) {
+        likeClicked = false;
+    }
+}
+
+
