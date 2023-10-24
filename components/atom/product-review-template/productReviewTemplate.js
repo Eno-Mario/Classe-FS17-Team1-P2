c173e5b169090fbbc9637db53a7f406f3bc1576c
Eno-Mario <141042374+Eno-Mario@users.noreply.github.com>
2023-10-24 20:50:58 +0200

    fix: like and dislike filled color in active mode

--- components/atom/product-review-template/productReviewTemplate.js
+++ components/atom/product-review-template/productReviewTemplate.js
@@ -2,44 +2,43 @@ var likeClicked = false; // Variabile per tenere traccia se il pulsante Like è 
 var dislikeClicked = false; // Variabile per tenere traccia se il pulsante Dislike è stato cliccato
 
 function pushLike() {
-    var numLikeElement = document.getElementsByClassName('numLike')[0];
-    var numLike = parseInt(numLikeElement.textContent);
-    var likeSvg = document.getElementsByClassName('likeSvg')
+  var numLikeElement = document.getElementsByClassName("numLike")[0];
+  var numLike = parseInt(numLikeElement.textContent);
+  var likeSvg = document.querySelector(".likeSvg");
 
-    if (!likeClicked) {
-        numLikeElement.textContent = numLike + 1;
-        likeClicked = true;
-        likeSvg.classList.add('active')
+  if (!likeClicked) {
+    numLikeElement.textContent = numLike + 1;
+    likeClicked = true;
+    likeSvg.classList.add("active");
+  } else {
+    numLikeElement.textContent = numLike - 1;
+    likeClicked = false;
+    likeSvg.classList.remove("active");
+  }
 
-        
-    } else {
-        numLikeElement.textContent = numLike - 1;
-        likeClicked = false;
-    }
-
-    // Resetta dislikeClicked dopo un clic su Like
-    if (dislikeClicked) {
-        dislikeClicked = false;
-    }
+  // Resetta dislikeClicked dopo un clic su Like
+  if (dislikeClicked) {
+    dislikeClicked = false;
+  }
 }
 
 function pushDislike() {
-    var numDislikeElement = document.getElementsByClassName('numDislike')[0];
-    var numDislike = parseInt(numDislikeElement.textContent);
+  var numDislikeElement = document.getElementsByClassName("numDislike")[0];
+  var numDislike = parseInt(numDislikeElement.textContent);
+  var dislikeSvg = document.querySelector(".dislikeSvg");
 
-    if (!dislikeClicked) {
-        numDislikeElement.textContent = numDislike + 1;
-        dislikeClicked = true;
+  if (!dislikeClicked) {
+    numDislikeElement.textContent = numDislike + 1;
+    dislikeClicked = true;
+    dislikeSvg.classList.add("active");
+  } else {
+    numDislikeElement.textContent = numDislike - 1;
+    dislikeClicked = false;
+    dislikeSvg.classList.remove("active");
+  }
 
-    } else {
-        numDislikeElement.textContent = numDislike - 1;
-        dislikeClicked = false;
-    }
-
-    // Resetta likeClicked dopo un clic su Dislike
-    if (likeClicked) {
-        likeClicked = false;
-    }
+  // Resetta likeClicked dopo un clic su Dislike
+  if (likeClicked) {
+    likeClicked = false;
+  }
 }
-
-
