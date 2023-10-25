9ec169d2b3668dd4a92bf966a2b263698528367e
Eno-Mario <141042374+Eno-Mario@users.noreply.github.com>
2023-10-25 14:52:42 +0200

    feat: added components review and card 2 in design system

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,6 +6,9 @@
 
 const standardButton = document.querySelectorAll(".standard-button");
 
+var likeClicked = false; // Variabile per tenere traccia se il pulsante Like è stato cliccato
+var dislikeClicked = false; // Variabile per tenere traccia se il pulsante Dislike è stato cliccato
+
 /* -----------------------
         LOGICA
 --------------------------*/
@@ -43,6 +46,50 @@ function toggleVignetta() {
 
 function closeVignetta() {
   document.getElementById("vignetta").style.display = "none";
+}
+
+function pushLike() {
+  var numLikeElement = document.getElementsByClassName("numLike")[0];
+  var numLike = parseInt(numLikeElement.textContent);
+  var likeSvg = document.querySelector(".likeSvg");
+
+  if (!likeClicked) {
+    numLikeElement.textContent = numLike + 1;
+    likeClicked = true;
+    likeSvg.classList.add("active");
+  } else {
+    numLikeElement.textContent = numLike - 1;
+    likeClicked = false;
+    likeSvg.classList.remove("active");
+  }
+
+  // Resetta dislikeClicked dopo un clic su Like
+  if (dislikeClicked) {
+    dislikeClicked = false;
+  }
+}
+
+//Product Review
+
+function pushDislike() {
+  var numDislikeElement = document.getElementsByClassName("numDislike")[0];
+  var numDislike = parseInt(numDislikeElement.textContent);
+  var dislikeSvg = document.querySelector(".dislikeSvg");
+
+  if (!dislikeClicked) {
+    numDislikeElement.textContent = numDislike + 1;
+    dislikeClicked = true;
+    dislikeSvg.classList.add("active");
+  } else {
+    numDislikeElement.textContent = numDislike - 1;
+    dislikeClicked = false;
+    dislikeSvg.classList.remove("active");
+  }
+
+  // Resetta likeClicked dopo un clic su Dislike
+  if (likeClicked) {
+    likeClicked = false;
+  }
 }
 
 /* ---------------------
