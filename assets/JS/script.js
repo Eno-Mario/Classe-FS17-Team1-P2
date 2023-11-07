857b3c29d8d5cfde029bfdd50a0f45c400fc3d16
Eno-Mario <141042374+Eno-Mario@users.noreply.github.com>
2023-11-07 19:44:02 +0100

    bugfix: homepage composizione parziale funzionante

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -84,6 +84,7 @@ const carousel = document.querySelectorAll(".carousel");
 const prevButton = document.querySelectorAll(".prev-button");
 const nextButton = document.querySelectorAll(".next-button");
 
+//header
 const container = document.querySelector(".carousel-atom-header");
 const element1 = document.getElementById("first-element-atom-header");
 const element2 = document.getElementById("second-element-atom-header");
@@ -111,7 +112,9 @@ const natale = document.querySelector(
 
 const bodyHeader = document.querySelector("body");
 
-console.log(natale);
+let menuPopUpCreated = false;
+
+//cookie
 
 const cookieButton = document.querySelector(".homepage-cookie-button");
 const body = document.querySelector("body");
@@ -578,6 +581,202 @@ buttonXHambMenu.addEventListener("click", () => {
   if (buttonXHambMenu) {
     containerAside.classList.add("not-visible-element-atom-header");
   }
+});
+
+/*/////////evento per tasto acquista /////////////////////// */
+
+const divForButton2 = document.createElement("div");
+divForButton2.setAttribute("class", "delete-button-pop-up-menu");
+const buttonXPopUpMenu2 = document.createElement("button");
+const image2 = document.createElement("img");
+image2.src = "close.png";
+image2.alt = "";
+buttonXPopUpMenu2.appendChild(image2);
+divForButton2.appendChild(buttonXPopUpMenu2);
+
+const containerPopMenu = document.createElement("div");
+const containerForMenuPopUp = document.createElement("div");
+containerForMenuPopUp.innerHTML = "";
+// Crea il contenuto di containerForMenuPopUp
+acquista.addEventListener("click", () => {
+  if (!menuPopUpCreated) {
+    // Crea il container per il menu pop-up
+    containerPopMenu.classList.add("not-visible-element-atom-header");
+
+    // Crea il container per il contenuto del menu pop-up
+
+    containerForMenuPopUp.className = "container-for-menu-pop-up-header";
+    containerPopMenu.classList.add("general-container-pop-up-menu");
+    containerPopMenu.classList.remove("not-visible-element-atom-header");
+    containerForMenuPopUp.innerHTML = `
+    <div class="content-pop-up-menu">
+        <ul>
+            <li><button><div class="all-arrow-flex-button"><span class="span-arrow-button-pop-up-menu">Set per tema</span><img src="arrow.svg" class="arrow-header-pop-up-menu"></img></div></button></li>
+            <li><button><div class="all-arrow-flex-button"><span class="span-arrow-button-pop-up-menu">Età</span><img src="arrow.svg" class="arrow-header-pop-up-menu"></img></div></button></li>
+            <li><button><div class="all-arrow-flex-button"><span class="span-arrow-button-pop-up-menu">Prezzi</span><img src="arrow.svg" class="arrow-header-pop-up-menu"></img></div></button></li>
+            <li><button><div class="all-arrow-flex-button"><span class="span-arrow-button-pop-up-menu">Merchandising LEGO</span><img src="arrow.svg" class="arrow-header-pop-up-menu"></img></div></button></li>
+            <li><button><div class="all-arrow-flex-button"><span class="span-arrow-button-pop-up-menu">Interessi</span><img src="arrow.svg" class="arrow-header-pop-up-menu"></img></div></button></li>
+            <li><button><div class="all-arrow-flex-button"><span class="span-arrow-button-pop-up-menu">Pick and Build</span><img src="arrow.svg" class="arrow-header-pop-up-menu"></img></div></button></li>
+            <li><div><a href="#">Set Esclusivi</a></div></li>
+            <li><div><a href="#">Novità</a></div></li>
+            <li><div><a href="#">I più venduti</a></div></li>
+            <li><div><a href="#">Home decor</a></div></li>
+            <li><div><a href="#">Sconti e offerte</a></div></li>
+            <li><div><a href="#">Buoni regalo</a></div></li>
+            <li><div><a href="#">Disponibile a breve</a></div></li>
+            <li><div><a href="#">Ultima occasione per acquistarli</a></div></li>
+        </ul>
+    </div>
+`;
+    containerForMenuPopUp.appendChild(divForButton2);
+    containerPopMenu.append(containerForMenuPopUp);
+    bodyHeader.append(containerPopMenu);
+    // Gestisci la chiusura del menu quando "buttonXHambMenu" viene cliccato
+
+    menuPopUpCreated = true;
+  }
+
+  buttonXPopUpMenu2.addEventListener("click", () => {
+    containerPopMenu.remove();
+    menuPopUpCreated = false;
+  });
+  // Gestisci la chiusura del menu quando containerPopMenu viene cliccato
+  containerPopMenu.addEventListener("click", (event) => {
+    if (event.target === containerPopMenu) {
+      containerPopMenu.remove();
+      menuPopUpCreated = false;
+    }
+  });
+
+  // Rimuovi il menu quando la finestra viene ridimensionata
+  window.addEventListener("resize", () => {
+    const containerPopMenu = document.querySelector(
+      ".general-container-pop-up-menu"
+    );
+    if (window.innerWidth < 901) {
+      containerPopMenu.remove();
+      menuPopUpCreated = false;
+    }
+  });
+});
+
+/*/////////evento per tasto scopri /////////////////////// */
+
+// Crea il contenuto di containerForMenuPopUp
+scopri.addEventListener("click", () => {
+  if (!menuPopUpCreated) {
+    // Crea il container per il menu pop-up
+    containerPopMenu.classList.add("not-visible-element-atom-header");
+
+    // Crea il container per il contenuto del menu pop-up
+
+    containerForMenuPopUp.className = "container-for-menu-pop-up-header";
+    containerPopMenu.classList.add("general-container-pop-up-menu");
+    containerPopMenu.classList.remove("not-visible-element-atom-header");
+    containerForMenuPopUp.innerHTML = `
+    <div class="content-pop-up-menu">
+        <ul>
+            <li><button><div class="all-arrow-flex-button"><span class="span-arrow-button-pop-up-menu">I nostri valori</span><img src="arrow.svg" class="arrow-header-pop-up-menu"></img></div></button></li>
+            <li><button><div class="all-arrow-flex-button"><span class="span-arrow-button-pop-up-menu">Le nostre app</span><img src="arrow.svg" class="arrow-header-pop-up-menu"></img></div></button></li>
+            <li><button><div class="all-arrow-flex-button"><span class="span-arrow-button-pop-up-menu">Le nostre riviste</span><img src="arrow.svg" class="arrow-header-pop-up-menu"></img></div></button></li>
+            <li><div><a href="#">Tutti i temi LEGO</a></div></li>
+            <li><div><a href="#">Tutti gli interessi LEGO</a></div></li>
+            <li><div><a href="#">Per gli adulti</a></div></li>
+            <li><div><a href="#">Per le famiglie</a></div></li>
+            <li><div><a href="#">LEGO® Insiders</a></div></li>
+            <li><div><a href="#">LEGO® Mosaic Maker</a></div></li>
+            <li><div><a href="#">Idee regalo LEGO®</a></div></li>
+        </ul>
+    </div>
+`;
+    containerForMenuPopUp.appendChild(divForButton2);
+    containerPopMenu.append(containerForMenuPopUp);
+    bodyHeader.append(containerPopMenu);
+    // Gestisci la chiusura del menu quando "buttonXHambMenu" viene cliccato
+
+    menuPopUpCreated = true;
+  }
+
+  buttonXPopUpMenu2.addEventListener("click", () => {
+    containerPopMenu.remove();
+    menuPopUpCreated = false;
+  });
+  // Gestisci la chiusura del menu quando containerPopMenu viene cliccato
+  containerPopMenu.addEventListener("click", (event) => {
+    if (event.target === containerPopMenu) {
+      containerPopMenu.remove();
+      menuPopUpCreated = false;
+    }
+  });
+
+  // Rimuovi il menu quando la finestra viene ridimensionata
+  window.addEventListener("resize", () => {
+    const containerPopMenu = document.querySelector(
+      ".general-container-pop-up-menu"
+    );
+    if (window.innerWidth < 901) {
+      containerPopMenu.remove();
+      menuPopUpCreated = false;
+    }
+  });
+});
+
+/*/////////evento per tasto aiuto /////////////////////// */
+
+// Crea il contenuto di containerForMenuPopUp
+aiuto.addEventListener("click", () => {
+  if (!menuPopUpCreated) {
+    // Crea il container per il menu pop-up
+    containerPopMenu.classList.add("not-visible-element-atom-header");
+
+    // Crea il container per il contenuto del menu pop-up
+
+    containerForMenuPopUp.className = "container-for-menu-pop-up-header";
+    containerPopMenu.classList.add("general-container-pop-up-menu");
+    containerPopMenu.classList.remove("not-visible-element-atom-header");
+    containerForMenuPopUp.innerHTML = `
+    <div class="content-pop-up-menu">
+        <ul>
+            <li><div><a href="#">Verifica stato ordine</a></div></li>
+            <li><div><a href="#">Consegne e resi</a></div></li>
+            <li><div><a href="#">Trova uno store</a></div></li>
+            <li><div><a href="#">Istruzioni per la costruzione</a></div></li>
+            <li><div><a href="#">Domande frequenti</a></div></li>
+            <li><div><a href="#">Contattaci</a></div></li>
+            <li><div><a href="#">Pezzi di ricambio</a></div></li>
+        </ul>
+    </div>
+`;
+    containerForMenuPopUp.appendChild(divForButton2);
+    containerPopMenu.append(containerForMenuPopUp);
+    bodyHeader.append(containerPopMenu);
+    // Gestisci la chiusura del menu quando "buttonXHambMenu" viene cliccato
+
+    menuPopUpCreated = true;
+  }
+
+  buttonXPopUpMenu2.addEventListener("click", () => {
+    containerPopMenu.remove();
+    menuPopUpCreated = false;
+  });
+  // Gestisci la chiusura del menu quando containerPopMenu viene cliccato
+  containerPopMenu.addEventListener("click", (event) => {
+    if (event.target === containerPopMenu) {
+      containerPopMenu.remove();
+      menuPopUpCreated = false;
+    }
+  });
+
+  // Rimuovi il menu quando la finestra viene ridimensionata
+  window.addEventListener("resize", () => {
+    const containerPopMenu = document.querySelector(
+      ".general-container-pop-up-menu"
+    );
+    if (window.innerWidth < 901) {
+      containerPopMenu.remove();
+      menuPopUpCreated = false;
+    }
+  });
 });
 
 // footer ------------------------------------------------------
