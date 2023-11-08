9d0b77dc5131ff498ce776def44fb93aa9517a36
Stef533 <137123241+Stef533@users.noreply.github.com>
2023-11-08 12:30:34 +0100

    create storeTop component

---
+++ components/store/store-top/storeTop.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   var toggleButton = document.querySelector('.toggle-button');
+//   var textNews = document.querySelector('.text-news');
+
+//   toggleButton.addEventListener('click', function() {
+//     if (textNews.classList.contains('show-all')) {
+//       textNews.classList.remove('show-all');
+//       toggleButton.textContent = 'Leggi di più';
+//     } else {
+//       textNews.classList.add('show-all');
+//       toggleButton.textContent = 'Mostra meno';
+//     }
+//   });
+// });
