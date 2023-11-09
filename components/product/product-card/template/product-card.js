36b5b410c69ef6279c18397b82596b7123c15625
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-11-09 16:17:29 +0100

    component :product-card

---
+++ components/product/product-card/template/product-card.js
@@ -0,0 +1,20 @@
+/* -----------------------
+    FASE DI PREPARAZIONE
+-------------------------- */
+var swiper = new Swiper(".mySwiper", {
+        navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+        },
+});
+/* -----------------------
+        LOGICA
+--------------------------*/
+
+/* ----------------------
+        FUNZIONI
+-------------------------*/
+
+/* ---------------------
+        EVENTI
+-----------------------*/
